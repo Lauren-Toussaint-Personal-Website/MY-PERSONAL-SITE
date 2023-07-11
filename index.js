e6cdccb1d903da70ac15bc65b3253a0b76f4be5b
Lauren August Toussaint <96217271+la-glitch@users.noreply.github.com>
2023-07-11 11:51:07 -0400

    part1

---
+++ index.js
@@ -0,0 +1,2 @@
+const str1= "Welcome to my site. I hope you find some interesting facts and information about me. Please let me know if you have further questions or wish to collaborate by contacting me. My contact information is listed below or you may complete a contact form available on the 'CONTACT' tab.";
+alert(`${str1}`)
